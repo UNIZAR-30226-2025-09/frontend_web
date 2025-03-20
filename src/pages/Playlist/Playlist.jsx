2b680d33c56ecbbc0fe62986bc78618010fa7e9e
Dim8DGH <868573@unizar.es>
2025-03-20 22:40:29 +0100

    pruebas del boton de like

--- src/pages/Playlist/Playlist.jsx
+++ src/pages/Playlist/Playlist.jsx
@@ -84,7 +84,7 @@ const PlaylistContent = () => {
                 const likeData = await likeResponse.json();
                 setIsLiked(likeData.isLiked);
             } catch (error) {
-                console.error("Error al obtener la playlist:", error);
+                console.json("Error al obtener la playlist:", error);
             }
         };
 
